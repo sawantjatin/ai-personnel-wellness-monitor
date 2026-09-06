74be2e523b9604ddf1480cc0fa8a5088c7059031
sawantjatin <jatinsawant02@gmail.com>
2026-09-06 23:13:42 +0530

    Add host configuration to Vite preview settings

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
+
   preview: {
+    host: "0.0.0.0",
     allowedHosts: ["ai-personnel-wellness-monitor.onrender.com"],
   },
 });
